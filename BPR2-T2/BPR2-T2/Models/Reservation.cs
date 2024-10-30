6d0ef69a25b3fd59c3a4840b7ee8ffb85449ac62
Huti <hozyfa954@gmail.com>
2024-10-30 13:28:14 +0100

    1st feat T2
    Project setup, connect T1 & T2, reservation feat, adding dockerfile

---
+++ BPR2-T2/BPR2-T2/Models/Reservation.cs
@@ -0,0 +1,14 @@
+namespace BPR2_T2.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public string GuestName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Company { get; set; }
+    public string Email { get; set; }
+    public string Comments { get; set; }
+    public DateTime Date { get; set; }
+    public string Time { get; set; }
+    public int NumOfPeople { get; set; }
+}
