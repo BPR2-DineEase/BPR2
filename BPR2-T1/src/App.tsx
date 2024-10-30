31fb6fa1ea2c25bd13306eca84c6d5e8c3222492
Luka Udovicic <lukaudovicic10@gmail.com>
2024-10-30 12:26:22 +0100

    build: added dev branch

--- BPR2-T1/src/App.tsx
+++ BPR2-T1/src/App.tsx
@@ -3,7 +3,7 @@ import TableReservation from "./components/TableReservation";
 function App() {
   return (
     <div className="h-full w-full">
-      <TableReservation />
+      <TableReservation />  
     </div>
   );
 }
