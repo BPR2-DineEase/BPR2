00ac08305d5eb2a88a23cb692671482819d614ff
Huti <hozyfa954@gmail.com>
2024-11-18 13:57:35 +0100

    Refactor ReservationContext

--- BPR2-T2/EfcDataAccess/Context/ReservationContext.cs
+++ BPR2-T2/EfcDataAccess/Context/ReservationContext.cs
@@ -1,17 +1,26 @@
 using Domain.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace EfcDataAccess.Context;
 
 public class ReservationContext : DbContext
 {
+    private readonly IConfiguration _configuration;
+
+    public ReservationContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
     public DbSet<Reservation> Reservations { get; set; }
     public DbSet<Restaurant> Restaurants { get; set; }
 
-
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite("Data Source =/app/Reservation.db");
+        
+        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseSqlite(connectionString);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
