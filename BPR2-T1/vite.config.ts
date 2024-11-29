e5e9285342071aa12a5c22402fe0a5628fd85312
Luka Udovicic <lukaudovicic10@gmail.com>
2024-11-29 11:26:48 +0100

    changed the port to 80

--- BPR2-T1/vite.config.ts
+++ BPR2-T1/vite.config.ts
@@ -10,8 +10,8 @@ export default defineConfig({
     },
   },
   server: {
-    host: "0.0.0.0", 
-    port: 5173,      
-    strictPort: true 
+    host: "0.0.0.0",
+    port: 80,
+    strictPort: true
   },
 });
